f186db3790ed90e41d17751f790091774e28aa74
MarijanaHornjak <marijanahornjak95@gmail.com>
2024-01-22 15:23:01 +0100

    added autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 // Code goes here!
 
+// Autobind decorator//
+
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+//////////////////////////////////////////////////
+
 class ProjectInput {
   //
   templateElement: HTMLTemplateElement;
@@ -33,7 +49,19 @@ class ProjectInput {
       "#people"
     ) as HTMLInputElement;
 
+    this.configure();
     this.attach();
+  }
+
+  //////////////
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  //////////////
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
